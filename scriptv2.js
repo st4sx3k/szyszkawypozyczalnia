0c5e63a9bb434ee219e62765fa04eb9c1522b50f
st4sx3k <113011314+st4sx3k@users.noreply.github.com>
2025-05-13 18:13:45 +0200

    Delete scriptv2.js

--- scriptv2.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggle = document.querySelector(".nav-toggle");
-    const navCenter = document.querySelector(".nav-center");
-
-    toggle.addEventListener("click", () => {
-      navCenter.classList.toggle("show");
-    });
-  });
